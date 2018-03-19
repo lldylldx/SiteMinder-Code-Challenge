08cd653f54c306c8d7362a162ac8e679207f9264
lldylldx <lldylldx@gmail.com>
2018-03-19 18:09:08 +1100

    Add files via upload
    make align with local disk

---
+++ configs/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Dependencies
+ */
+const
+    _ = require('lodash'),
+    env = process.env.NODE_ENV || 'local',
+    envConfig = require('./' + env);
+
+let defaultConfig = {
+    env: env
+};
+
+module.exports = _.merge(defaultConfig, envConfig);
---
+++ configs/local.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let localConfig = {
+    hostname: 'localhost',
+    port: 3000,
+    sendgridApiBaseURL: 'https://api.sendgrid.com/v3/',
+    mailgunApiBaseURL: 'https://api.mailgun.net/v3/',
+    sendgridApiKey: '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',
+    mailgunApiKey: 'key-a3a82031ea3836b401cfe33514b076da',
+    mailgunDomain: 'thesouthface.com.au'
+    //viewDir: './app/views'
+};
+
+module.exports = localConfig;
